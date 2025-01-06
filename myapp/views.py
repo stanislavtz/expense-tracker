abba12da5d9b89cf3fefd5e7796468dd586d9be3
Tzekov <stanislav.tzekov@dxc.com>
2025-01-06 20:07:46 +0200

    Render expense form and table that includes all expenses.

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import get_list_or_404
+from .forms import ExpenseForm
+from .models import Expense
 
 # Create your views here.
 def index(request):
-    return render(request, "myapp/index.html")
+    form = ExpenseForm()
+
+    if request.method == "POST":
+        form = ExpenseForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+        
+    all_expenses = get_list_or_404(Expense)
+
+    context = {
+        "form": form,
+        "all_expenses": all_expenses,
+    }
+
+    return render(request, "myapp/index.html", context)
