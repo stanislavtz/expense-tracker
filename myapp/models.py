ebd21aa6a88f777c1c9dee1c5d17f78268a05ef9
Tzekov <stanislav.tzekov@dxc.com>
2025-01-06 13:07:10 +0200

    Add Expense model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.PositiveIntegerField()
+    category = models.CharField(max_length=50)
+    created = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
