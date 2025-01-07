63db3f754db06287e4278df28d35444c3c5f6e2d
Tzekov <stanislav.tzekov@dxc.com>
2025-01-07 11:22:35 +0200

    Add edit expense functionality.

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404
 from .forms import ExpenseForm
 from .models import Expense
 
@@ -22,3 +22,22 @@ def index(request):
     }
 
     return render(request, "myapp/index.html", context)
+
+
+def edit_expense(request, expense_id):
+    current_expense = get_object_or_404(Expense, id=expense_id)
+    edit_form = ExpenseForm(instance=current_expense)
+
+    if request.method == "POST":
+        edit_form = ExpenseForm(request.POST, instance=current_expense)
+
+        if edit_form.is_valid():
+            edit_form.save()
+            return redirect("index")
+
+    context = {
+        "expense": current_expense,
+        "form": edit_form,
+    }
+
+    return render(request, "myapp/edit_expense.html", context)
