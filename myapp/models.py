6eaae04c3d4b0d8288d67b4b5e6fa8a8d95104d7
Tzekov <stanislav.tzekov@dxc.com>
2025-01-09 17:04:39 +0200

    Add user to Expense model.

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Expense(models.Model):
     name = models.CharField(max_length=255)
-    amount = models.PositiveIntegerField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=50)
     created = models.DateField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="expenses")
 
     def __str__(self):
         return self.name
