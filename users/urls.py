67b91a7cc3a3d053811e1e6061895170ede9c514
Tzekov <stanislav.tzekov@dxc.com>
2025-01-09 13:02:10 +0200

    Implement login functionality.

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("register/", views.register, name="register"),
+    path("login/", views.login_user, name="login"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from .forms import RegisterForm
+from django.contrib.auth import login, authenticate
+from .forms import LoginForm, RegisterForm
 
 # Create your views here.
 def register(request):
@@ -27,8 +28,8 @@ def register(request):
                     last_name=last_name,
                     password=password
                 )
-                
-                return redirect("index")
+
+                return redirect("login")
         else:
             message = "Please fill all input fields with valid data."
            
@@ -38,3 +39,35 @@ def register(request):
     }
 
     return render(request, "users/register.html", context)
+
+
+def login_user(request):
+    login_form = LoginForm()
+    message = None
+
+    if request.user.is_authenticated:
+        return redirect("index")
+
+    if request.method == "POST":
+        login_form = LoginForm(request.POST)
+        
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get("username")
+            password = login_form.cleaned_data.get("password")
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user=user)
+                return redirect("index")
+            else:
+                message = "Invalid username or password!"
+        else:
+            message = "Please fill all input fields with valid data."
+
+    context = {
+        "form": login_form,
+        "message": message
+    }
+
+    return render(request, "users/login.html", context)
